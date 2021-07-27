2588a79104d5f50f3fb91ed9de3052e81a656776
Pedro Henrique Pereira Almeida <pedro.6571almeida@gmail.com>
2021-07-26 21:24:19 -0300

    Alter Table - Add Constraint

---
+++ 02 - Script de alteracao de tabelas, inserindo constraints para adicionar FKs.sql
@@ -0,0 +1,20 @@
+--Alterando a tabela Turmas
+alter table Turmas
+	add constraint fk_alunos foreign key (id_aluno) references Alunos (id_aluno);
+
+alter table Turmas
+	add constraint fk_cursos foreign key (id_curso) references Cursos (id_curso);
+
+--Alterando a tabela Registro_Presenca
+alter table Registro_Presenca
+	add constraint fk_TurmasRP foreign key (id_turma) references Turmas (id_turma);
+
+alter table Registro_Presenca
+	add constraint fk_AlunoRP foreign key (id_aluno) references Alunos (id_aluno);
+
+alter table Registro_Presenca
+	add constraint fk_SituacaoRP foreign key (id_situacao) references Situacao (id_situacao);
+
+/*alter table Alunos
+
+	drop fk_Turmas;*/ --Excluir constraint
